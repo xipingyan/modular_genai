4987a263b1986af7ccc417778515ab34a42fba6a
xiping.yan <xiping.yan@intel.com>
2025-12-16 08:59:00 +0800

    build on ubuntu24

--- README.md
+++ README.md
@@ -32,8 +32,12 @@ cmake -DCMAKE_INSTALL_PREFIX=install ..
 make -j20 && make install
 
 <!-- OR download 2025.4 (default) -->
+<!-- ubuntu22 -->
 wget https://storage.openvinotoolkit.org/repositories/openvino/packages/2025.4/linux/openvino_toolkit_ubuntu22_2025.4.0.20398.8fdad55727d_x86_64.tgz
 tar -xf openvino_toolkit_ubuntu22_2025.4.0.20398.8fdad55727d_x86_64.tgz
+<!-- ubuntu24 -->
+wget https://storage.openvinotoolkit.org/repositories/openvino/packages/2025.4/linux/openvino_toolkit_ubuntu24_2025.4.0.20398.8fdad55727d_x86_64.tgz
+tar -xf openvino_toolkit_ubuntu24_2025.4.0.20398.8fdad55727d_x86_64.tgz
 
 <!-- Update source_ov.sh based on your OV. -->
 ```
--- source_ov.sh
+++ source_ov.sh
@@ -12,9 +12,9 @@ if [ $USE_NIGHT_OV = "1" ]; then
     if [ "$OS_VERSION" = "22.04" ]; then
         source ./openvino_toolkit_ubuntu22_2025.4.0.20398.8fdad55727d_x86_64/setupvars.sh
     elif [ "$OS_VERSION" = "24.04" ]; then
-        source ./openvino_toolkit_ubuntu22_2025.4.0.20398.8fdad55727d_x86_64/setupvars.sh
+        source ./openvino_toolkit_ubuntu24_2025.4.0.20398.8fdad55727d_x86_64/setupvars.sh
     elif [ "$OS_VERSION" = "24.10" ]; then
-        source ./openvino_toolkit_ubuntu22_2025.4.0.20398.8fdad55727d_x86_64/setupvars.sh
+        source ./openvino_toolkit_ubuntu24_2025.4.0.20398.8fdad55727d_x86_64/setupvars.sh
     else
         echo "Error: Can't support version of Ubuntu: $OS_VERSION"
     fi
