45578eefc1d4cb3b53059bc0334edcb547fcc6bf
xipingya <xiping.yan@intel.com>
2026-03-10 14:58:10 +0800

    update new-arch ov path

--- build_genai.sh
+++ build_genai.sh
@@ -20,7 +20,7 @@ cd openvino.genai
 echo "================================"
 echo "== Start to build OpenVINO GenAI with new arch OpenVINO."
 
-OV_PATH=/home/xiping/mygithub/openvino-new-arch/openvino/build
+OV_PATH=$SCRIPT_DIR_BUILD_GENAI/../openvino-new-arch/openvino/build
 # cmake -DCMAKE_BUILD_TYPE=Debug -DENABLE_DYNAMIC_LOAD_MODEL_WEIGHTS=OFF -DENABLE_OPENVINO_NEW_ARCH=ON -DOpenVINO_DIR=$OV_PATH -S ./ -B ./build/
 cmake --build ./build/ --config Debug -j 200
 cmake --install ./build/ --config Debug --prefix ./install
