b7939aae8627aa86429b6fdc13f24388fc7f7e1d
xiping.yan <xiping.yan@intel.com>
2026-04-08 15:07:08 +0800

    update build script of composable pipeline .
    Signed-off-by: xiping.yan <xiping.yan@intel.com>

--- build_composable_pipeline.sh
+++ build_composable_pipeline.sh
@@ -12,18 +12,8 @@ cd composable_pipeline
 # BUILD_TYPE=Release
 BUILD_TYPE=Debug
 
-# Build OpenVINO GenAI submodule (required for extension ops like SpecialTokensSplit)
-GENAI_ROOT="${SCRIPT_DIR_BUILD_GENAI}/composable_pipeline/thirdparty/openvino.genai"
-GENAI_BUILD="${GENAI_ROOT}/build"
-GENAI_INSTALL="${GENAI_BUILD}/install"
-
-cmake -S "${GENAI_ROOT}" -B "${GENAI_BUILD}" -DCMAKE_BUILD_TYPE="$BUILD_TYPE" -DOpenVINO_DIR="$OV_PATH"
-cmake --build "${GENAI_BUILD}" --config "$BUILD_TYPE" -j 30
-cmake --install "${GENAI_BUILD}" --config "$BUILD_TYPE" --prefix "${GENAI_INSTALL}"
-
-echo "================================"
-echo "== Start to build OpenVINO GenAI with new arch OpenVINO."
-# cmake --preset minimal -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DOpenVINO_DIR=$OV_PATH -DENABLE_MODELING_PRIVATE=ON 
-cmake --preset samples -DCMAKE_BUILD_TYPE="$BUILD_TYPE" -DOpenVINO_DIR="$OV_PATH" -DENABLE_CB=ON -DOpenVINOGenAI_DIR="${GENAI_INSTALL}/runtime/cmake" -DENABLE_MODELING_PRIVATE=ON 
+# echo "================================"
+# echo "== Start to build OpenVINO GenAI with new arch OpenVINO."
+cmake --preset full -DCMAKE_BUILD_TYPE="$BUILD_TYPE" -DOpenVINO_DIR="$OV_PATH" -B build
 cmake --build build --config "$BUILD_TYPE" -j 30
 cmake --install ./build/ --config "$BUILD_TYPE" --prefix ./build/install
