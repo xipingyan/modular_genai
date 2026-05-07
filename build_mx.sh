ad1a2d9080461960bfb8cf6deef715981d3b24b0
xiping.yan <xiping.yan@intel.com>
2026-05-07 16:17:32 +0800

    enable build mx script

---
+++ build_mx.sh
@@ -0,0 +1,33 @@
+
+set -eo pipefail
+
+SCRIPT_DIR_BUILD_GENAI="$(dirname "$(readlink -f "$BASH_SOURCE")")"
+cd "${SCRIPT_DIR_BUILD_GENAI}"
+
+source ./python-env/bin/activate
+source ./source_mx_ov.sh
+
+echo "${SCRIPT_DIR_BUILD_GENAI}"
+
+cd openvino.mx
+
+BUILD_TYPE=Release
+# BUILD_TYPE=Debug
+
+# echo "================================"
+# echo "== Start to build OpenVINO GenAI with new arch OpenVINO."
+# -DCMAKE_BUILD_TYPE="$BUILD_TYPE"
+
+# IMPORTANT:
+# Force composable_pipeline to build openvino.genai from submodule source.
+# Otherwise CMake may find a cached/prebuilt OpenVINOGenAI package and skip
+# recompiling changes under thirdparty/openvino.genai/src/...
+unset OpenVINOGenAI_DIR
+cmake --preset full -DOpenVINO_DIR=$OV_PATH_CMAKE -B build -DOpenCV_DIR=/usr/lib/x86_64-linux-gnu/cmake/opencv4 \
+	-DCMAKE_DISABLE_FIND_PACKAGE_OpenVINOGenAI=OFF \
+	-DOpenVINOGenAI_DIR=/__force_use_submodule_openvino_genai__
+
+# cmake --preset full -DOpenVINO_DIR=$OV_PATH_CMAKE -B build -DOpenCV_DIR=/usr/lib/x86_64-linux-gnu/cmake/opencv4
+
+cmake --build build --config "$BUILD_TYPE" -j 20
+cmake --install ./build/ --config "$BUILD_TYPE" --prefix ./build/install
---
+++ source_mx_ov.sh
@@ -0,0 +1,28 @@
+
+SCRIPT_MY_OV_DIR="$(dirname "$(readlink -f "$BASH_SOURCE")")"
+cd ${SCRIPT_MY_OV_DIR}
+
+source ./python-env/bin/activate
+
+USE_NIGHT_OV="0" # download from nightly build.
+
+if [ $USE_NIGHT_OV = "1" ]; then
+    echo "-------------- USE_NIGHTLY_OV"
+    OS_VERSION=$(lsb_release -rs)
+    if [ "$OS_VERSION" = "22.04" ]; then
+        source ./openvino_toolkit_ubuntu22_2025.4.0.20398.8fdad55727d_x86_64/setupvars.sh
+    elif [ "$OS_VERSION" = "24.04" ]; then
+        source ./openvino_toolkit_ubuntu24_2025.4.0.20398.8fdad55727d_x86_64/setupvars.sh
+    elif [ "$OS_VERSION" = "24.10" ]; then
+        source ./openvino_toolkit_ubuntu24_2025.4.0.20398.8fdad55727d_x86_64/setupvars.sh
+    else
+        echo "Error: Can't support version of Ubuntu: $OS_VERSION"
+    fi
+else
+    echo "-------------- Use my build OV"
+    export OV_PATH=$SCRIPT_MY_OV_DIR/openvino.mx/thirdparty/openvino
+    source ${OV_PATH}/build/install/setupvars.sh
+    export OV_PATH_BUILD=$OV_PATH/build
+    export OV_PATH_CMAKE=$OV_PATH/build/install/runtime/cmake
+fi
+
