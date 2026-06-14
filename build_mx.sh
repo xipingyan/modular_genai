253aae461130d469e3c6728ffd39ddb5f24bdae2
xipingya <xiping.yan@intel.com>
2026-06-14 13:17:16 +0800

    update build mx.
    Signed-off-by: xipingya <xiping.yan@intel.com>

--- build_mx.sh
+++ build_mx.sh
@@ -5,33 +5,16 @@ SCRIPT_DIR_BUILD_PIPELINE_MX="$(dirname "$(readlink -f "$BASH_SOURCE")")"
 cd "${SCRIPT_DIR_BUILD_PIPELINE_MX}"
 
 source ./python-env/bin/activate
-source ./source_mx_ov.sh
 
 echo "${SCRIPT_DIR_BUILD_PIPELINE_MX}"
 
 cd openvino.pipeline.mx
 
-# Download and prepare OpenVINO GenAI dependencies.
-python scripts/bootstrap_deps.py --stb
-
 BUILD_TYPE=Release
-BUILD_TYPE=Debug
+# BUILD_TYPE=Debug
 
-# echo "================================"
-# echo "== Start to build OpenVINO GenAI with new arch OpenVINO."
-# -DCMAKE_BUILD_TYPE="$BUILD_TYPE"
-
-# IMPORTANT:
-# Force composable_pipeline to build openvino.genai from submodule source.
-# Otherwise CMake may find a cached/prebuilt OpenVINOGenAI package and skip
-# recompiling changes under thirdparty/openvino.genai/src/...
-unset OpenVINOGenAI_DIR
-cmake --preset full -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DOpenVINO_DIR=$OV_PATH_CMAKE -B build -DOpenCV_DIR=/usr/lib/x86_64-linux-gnu/cmake/opencv4 \
-	-DCMAKE_DISABLE_FIND_PACKAGE_OpenVINOGenAI=OFF \
-	-DENABLE_PROFILING_ITT=ON \
-	-DOpenVINOGenAI_DIR=/__force_use_submodule_openvino_genai__
-
-# cmake --preset full -DOpenVINO_DIR=$OV_PATH_CMAKE -B build -DOpenCV_DIR=/usr/lib/x86_64-linux-gnu/cmake/opencv4
-
-cmake --build build --config "$BUILD_TYPE" -j 20
+python scripts/bootstrap_deps.py --openvino --build-dir build --build-type $BUILD_TYPE --stb
+# cmake --preset minimal
+cmake --preset full
+cmake --build build --config "$BUILD_TYPE" -j$(nproc)
 cmake --install ./build/ --config "$BUILD_TYPE" --prefix ./build/install
--- run_benchmark.sh
+++ run_benchmark.sh
@@ -8,6 +8,46 @@ cd "${SCRIPT_DIR_PIPELINE_BENCHMARK}/openvino.pipeline.mx"
 ${SCRIPT_DIR_PIPELINE_BENCHMARK}/source_mx_ov.sh
 source ${SCRIPT_DIR_PIPELINE_BENCHMARK}/openvino.pipeline.mx/build/install/setupvars.sh
 
-# pipeline_benchmark is now in PATH thanks to setupvars.sh
+# export ENABLE_PROFILE=1       # Dump profiling data. default 0.
+# export DUMP_YAML=1            # Dump pipeline to YAML file. default 0.
+# export DUMP_PERFORMANCE=1     # Dump performance metrics after generation. default 0.
+# export OPENVINO_LOG_LEVEL=3   # Set OpenVINO log level.
+
+# ===========================================================
+# Run benchmark: Qwen3-omni config with image input
+config_yaml=./samples/config_yaml/Qwen3_omni/config_image_cb.yaml
+# pipeline_benchmark $config_yaml "prompts=How many cars in the image?" "images=./tests/test_data/cars-1200-674.jpg" --warmup 1 --iter 2
+pipeline_benchmark $config_yaml "prompts=How many images input?" "images=./tests/test_data/cars-1200-674.jpg,./tests/test_data/cat_120_100.png" --warmup 1 --iter 1
+
+# config_yaml=./samples/config_yaml/Qwen3_omni/config_image_video_audio_cb.yaml
+# pipeline_benchmark $config_yaml "prompts=Transcribe the input audio." "audios=./tests/test_data/thunder-and-rain-sounds.wav" --warmup 0 --iter 1
+
+
+# # ===========================================================
+# # Run benchmark: Qwen3-omni config with chat input
+# config_yaml=./tests/data/tiny_models/tiny_llm_phi_2_chat_cb_with_yaml/config.yaml
+# chat_json=./samples/cpp/benchmark/examples/simple_chat.json
+# pipeline_benchmark $config_yaml --conversation "$chat_json" --warmup 1 --iter 2
+
+# ===========================================================
+# Run benchmark: Qwen3-omni config with chat input
 config_yaml=./samples/config_yaml/Qwen3_omni/config_chat_cb.yaml
-pipeline_benchmark $config_yaml "prompts=How many cars in the image?" "images=./tests/test_data/cars-1200-674.jpg"
+# chat_json=./samples/cpp/benchmark/examples/multimodal_chat.json
+# pipeline_benchmark $config_yaml --conversation "$chat_json" --warmup 0 --iter 1
+
+chat_json=./samples/cpp/benchmark/examples/case1_comprehensive.json
+echo "===> Running benchmark with conversation: ${chat_json}"
+pipeline_benchmark $config_yaml --conversation "$chat_json" --warmup 0 --iter 1
+chat_json=./samples/cpp/benchmark/examples/case2_video_audio_focus.json
+echo "===> Running benchmark with conversation: ${chat_json}"
+pipeline_benchmark $config_yaml --conversation "$chat_json" --warmup 0 --iter 1
+chat_json=./samples/cpp/benchmark/examples/image_comparison.json
+echo "===> Running benchmark with conversation: ${chat_json}"
+pipeline_benchmark $config_yaml --conversation "$chat_json" --warmup 0 --iter 1
+chat_json=./samples/cpp/benchmark/examples/multi_images_ordering.json
+echo "===> Running benchmark with conversation: ${chat_json}"
+pipeline_benchmark $config_yaml --conversation "$chat_json" --warmup 0 --iter 1
+
+chat_json=./samples/cpp/benchmark/examples/text_only_baseline.json
+echo "===> Running benchmark with conversation: ${chat_json}"
+pipeline_benchmark $config_yaml --conversation "$chat_json" --warmup 0 --iter 1
